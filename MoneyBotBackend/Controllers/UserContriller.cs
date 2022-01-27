5aff82fecd010acac7f4483b3b5f03fde1f0e96f
Am1go1 <ivantimofeev930@gmail.com>
2022-01-27 18:27:51 +0300

    Initial commit

---
+++ MoneyBotBackend/Controllers/UserContriller.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MoneyBotBackend.DbContext;
+using MoneyBotBackend.Models;
+using MoneyBotBackend.Models.Dto;
+
+namespace MoneyBotBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserContriller : ControllerBase
+    {
+        public MoneyBotContext _context;
+
+        public UserContriller(MoneyBotContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task <ActionResult> Regirter(UserIdentity userIdentity)
+        {
+            User user = new User() 
+            { 
+                PhoneNumberPrefix = userIdentity.PhoneNumberPrefix,
+                PhoneNumber = userIdentity.PhoneNumber,
+                Password = userIdentity.Password,
+                Moneys = new List<Money>()
+            };
+
+            _context.Users.Add(user);
+
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
---
+++ MoneyBotBackend/Startup.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using MoneyBotBackend.DbContext;
+
+namespace MoneyBotBackend
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            // Регистрация контекста в системе
+            services.AddDbContext<MoneyBotContext>(p => 
+                p.UseSqlite("Data Source=usersdata.db; Foreign Keys=True"));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
